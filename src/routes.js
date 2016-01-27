3bf52ad0f84b717e59b8f63e6303bcfd3b222034
kulakowka <kulakowka@gmail.com>
2016-01-28 00:58:58 +0300

    routes cleanup

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,12 @@ import App from './containers/App'
 import UserPage from './containers/UserPage'
 import RepoPage from './containers/RepoPage'
 
-export default function Routes() {
+export default function Routes () {
   return (
     <Router history={browserHistory}>
-      <Route path="/" component={App}>
-        <Route path="/:login/:name"
-               component={RepoPage} />
-        <Route path="/:login"
-               component={UserPage} />
+      <Route path='/' component={App}>
+        <Route path='/:login/:name' component={RepoPage} />
+        <Route path='/:login' component={UserPage} />
       </Route>
     </Router>
   )
